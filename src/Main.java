f04050721704df0f81fd40e5a2f533a3ce809d2c
teelair <tylerrifeasd@gmail.com>
2017-02-15 09:10:27 -0700

    Wow.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,11 +14,30 @@ public class Main
 	{
 		//2^31 - 1 = FXSHRXX
 		//2^63 - 1 = CRPXNLSKVLJDTX
-		parseStringBI("A");
-		parseStringBI("FXSHRXW");
-		parseStringBI("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+		//System.out.println(numberToCell(new BigInteger("839")));
+		//System.out.println(parseStringBI("CRPXNLSKVLJDTX"));
+		//System.out.println(parseStringBI("IWPJNFTCRUPGWTNYQJTMKGOXMAJVMDTYLCNYPWNRFLQFEIAETRQEMZGBBUMEUZAALDUEKDXXQRVKHKARVTQDHWWBSQLEVMSWMNVIBUGWPLAIHKTWTSNNNKOFRBILUOLQMCQAZKVZOJSIHZTJDXGQJPPNNZPSQADKGDYAINIUEJYRWFMTJYPMXNQALITDWETDQSVOSYYUNEKURDWFIMAQQWJEAYVZNCQTKMTOVVDCASEEUBMQMLMPVHTKWNDLNBDPDWZUSSRXEYYCWWYQJXRTWRNGPZKBLKXUBMMBBZXYZIKOLKBQZWPJQLOZUVEUGWTXUCUPZUWZTSQNOBTGGBMTUNLMEIQSEXKTCZDRTMHEJRVMESMAVPFBAKRMVZDFVUTYDWANNCJSJMIJGSMZTJZFNGBTUCAXJUJSOOAYYMMCNGCMESKSYQCV"));
+		//BigInteger largePrime = new BigInteger(largePrime);
+		try
+		{
+			FileReader fr = new FileReader("C:\\Users\\rifejt000\\Desktop\\Large Prime.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line = "";
+			PrintWriter pw = new PrintWriter("C:\\Users\\rifejt000\\Desktop\\Compressed Prime.txt");
+			while((line = br.readLine()) != null)
+			{
+				String output = numberToCell(new BigInteger(line));
+				pw.println(output);
+			}
+			pw.close();
+			br.close();
+		}catch(Exception e){}
+		//String output = numberToCell(new BigInteger("2").pow(2048));
+		
+		//	parseStringBI("A");
+		//parseStringBI("FXSHRXW");
 	}
-
+/**
 	public static void parseString(String input)
 	{
 		List<Character> charArray = new ArrayList<Character>();
@@ -31,8 +53,8 @@ public class Main
 		}
 		System.out.println(runningTotal);
 	}
-	
-	public static void parseStringBI(String input)
+*/
+	public static BigInteger parseStringBI(String input)
 	{
 		List<Character> charArray = new ArrayList<Character>();
 		for(char ch : input.toCharArray())
@@ -45,12 +67,67 @@ public class Main
 		{
 			runningTotal = runningTotal.add(new BigInteger("26").pow(i).multiply(new BigInteger((alphabet.indexOf(charArray.get(i)) + 1) + "")));
 		}
-		System.out.println(runningTotal);
+		return runningTotal;
+	}
+
+	public static String numberToCell(BigInteger number)
+	{
+		BigInteger upperBound = BigInteger.ZERO;
+		int power = 0;
+		while(upperBound.compareTo(number) == -1)
+		{
+			upperBound = upperBound.add(new BigInteger("26").pow(power));
+			power++;
+			System.out.println(upperBound + ", " + power);
+			if(upperBound.compareTo(number) == 0)
+			{
+				StringBuilder sb = new StringBuilder();
+				for(int i = 0; i < power; i++)
+				{
+					sb.append("A");
+				}
+				return sb.toString();
+			}
+		}
+		//we know the power, let's start converging.
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < (power - 1); i++)
+		{
+			sb.append("Z");
+		}
+		System.out.println(sb.toString());
+		return recursivelyConverge(number, sb.toString().toCharArray(), power - 1, 0);
 	}
 	
-	public static void numberToCell(int number)
+	public static String recursivelyConverge(BigInteger target, char[] resolved, int power, int lastIndex)
 	{
-		BigInteger guess = BigInteger.ZERO;
-		
+		System.out.println(lastIndex);
+		if(lastIndex > resolved.length - 1)
+		{
+			return new String(resolved);
+		}
+		BigInteger overallResolvedValue = parseStringBI(new String(resolved));
+		while(overallResolvedValue.compareTo(target) == 1)
+		{
+			//System.out.println(new String(resolved));
+			int charIndex = alphabet.indexOf(resolved[lastIndex]);
+			if(charIndex - 1 >= 0)
+			{
+				char[] lastResolved = resolved.clone();
+				resolved[lastIndex] = alphabet.charAt(charIndex - 1);
+				overallResolvedValue = parseStringBI(new String(resolved));
+				if(overallResolvedValue.compareTo(target) == -1)
+				{
+					return recursivelyConverge(target, lastResolved, power, lastIndex + 1);
+				}
+			}
+			else
+			{
+				resolved[lastIndex] = 'A';
+				return recursivelyConverge(target, resolved, power, lastIndex + 1);
+			}
+			overallResolvedValue = parseStringBI(new String(resolved));
+		}
+		return recursivelyConverge(target, resolved, power, lastIndex + 1);
 	}
 }
